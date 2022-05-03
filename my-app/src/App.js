1e3595f891354c672ae39b746bee7a95c20e9ad7
Murat Iskenov <morat.aisken@gmail.com>
2022-05-03 20:27:13 +0600

    added items jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 import './App.css'
 import Header from './components/Header/header'
+import Main from "./components/Main/main"
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      
+      <Main/>
     </div>
   );
 }
--- my-app/src/components/Main/main.jsx
+++ my-app/src/components/Main/main.jsx
@@ -1,4 +1,30 @@
 import './main.css'
-
-
-export default function Main
+import logo from '../Assets/Mainlogo.png'
+import pict from '../Assets/Pict.png'
+export default function Main(){
+    return(
+        <main>
+            <div className="text-container">
+                <ul id='horizontal-text'>
+                    <li>•Jersey</li>
+                    <li>•Nike & NBA Collaboration</li>
+                </ul>
+            </div>
+            <div className="contain">
+                <div className="titlecontain">
+                    <span>JERSEY</span>
+                    <img src={logo} alt="" />
+                    <span>NBA</span>
+                </div>
+                <div className="bottomcontain">
+                    <div className="textin">
+                    <p>WHICHEVER <span className='nba'>NBA</span> <span className="team">TEAM</span> YOUR SUPPORT, GET YOUR JERSEY HERE TODAY.</p>
+                    </div>
+                </div>
+                <div className="image">
+                        <img src={pict} alt="" />
+                    </div>
+            </div>
+        </main>
+    )
+}
